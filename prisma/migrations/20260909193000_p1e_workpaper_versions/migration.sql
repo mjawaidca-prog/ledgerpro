7cda613e3c25608713abb70c96dbab547f64e027
mjawaidca-prog <mjawaid.ca@gmail.com>
2026-09-09 13:27:06 -0600

    Use stable tax workpaper version index

--- prisma/migrations/20260909193000_p1e_workpaper_versions/migration.sql
+++ prisma/migrations/20260909193000_p1e_workpaper_versions/migration.sql
@@ -1,2 +1,2 @@
 DROP INDEX "TaxReturnWorkpaper_companyId_registrationId_periodStart_periodEnd_key";
-CREATE UNIQUE INDEX "TaxReturnWorkpaper_companyId_registrationId_periodStart_periodEnd_version_key" ON "TaxReturnWorkpaper"("companyId","registrationId","periodStart","periodEnd","version");
+CREATE UNIQUE INDEX "TaxWorkpaper_period_version_key" ON "TaxReturnWorkpaper"("companyId","registrationId","periodStart","periodEnd","version");
